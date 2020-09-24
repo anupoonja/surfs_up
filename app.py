8a47dd3bbdcea29ca209fce5c5affe27975cec33
Anusuya Poonja <68753330+anupoonja@users.noreply.github.com>
2020-09-24 00:13:10 -0500

    Flask app for Weather report

--- app.py
+++ app.py
@@ -1,48 +1,79 @@
-from flask import Flask
+import datetime as dt
+import numpy as np
+import pandas as pd
 
-# __name__ - variable (string)
-# Variable with underscore are magic maethods.
-# name of the file
-    # 1. python app.py - most typical
-    # 2. flask run - less obvious, not as common for new comers
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
 
-# __name__ - variable (string)
-    # 1. first type of execution -> __name__ = "__main__" (1)
-    # 2. second type of execution -> __name__ = $FLASK_APP
+from flask import Flask, jsonify
 
-# to run a flask app - Create Flask app
-# app. run() -> <app_name>.run()
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save references to each table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# Create our session (link) from Python to the DB
+session = Session(engine)
+
+# Create Flask application
 app = Flask(__name__)
 
-print("Hello anytime")
-print(__name__)
-
-# decorator - pass a function into a function
-# Starting point or the root
-# flask run : from cmd
 @app.route('/')
-def hello_world():
-    #return "Hello World"
-    return "<h1>Hello World</h1>"
+def welcome():
+    return(
+    '''
+    Welcome to the Climate Analysis API!
+    Available Routes:
+    /api/v1.0/precipitation
+    /api/v1.0/stations
+    /api/v1.0/tobs
+    /api/v1.0/temp/start/end
+    ''')
 
+@app.route('/api/v1.0/precipitation')
+def precipitation():
+   prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+   precipitation = session.query(Measurement.date, Measurement.prcp).\
+      filter(Measurement.date >= prev_year).all()
+   precip = {date: prcp for date, prcp in precipitation}
+   return jsonify(precip)
 
-# extra page eg : http://127.0.0.1:5000/extra or http://localhost:5000/extra
-@app.route("/extra")
-def extra():
-     return "<h1>Hello World 2</h1>"
+@app.route('/api/v1.0/stations')
+def stations():
+    results = session.query(Station.station).all()
+    stations = list(np.ravel(results))
+    return jsonify(stations=stations)
 
-# python app.py : from cmd line
-if __name__ == "__main__":
-    print("Hello from cmd line")
-    app.run(debug=True)
-    # app.run(debug=True)
-    # port=50000 host=
+@app.route('/api/v1.0/tobs')
+def temp_monthly():
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    results = session.query(Measurement.tobs).\
+    filter(Measurement.station == 'USC00519281').\
+    filter(Measurement.date >= prev_year).all()
+    temps = list(np.ravel(results))
+    return jsonify(temps=temps)
 
+@app.route("/api/v1.0/temp/<start>")
+@app.route("/api/v1.0/temp/<start>/<end>")
+def stats(start=None, end=None):
+    sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]           
 
+    if not end: 
+       results = session.query(*sel).\
+       filter(Measurement.date <= start).all()
+       temps = list(np.ravel(results))
+       return jsonify(temps)
 
-
-
-
-
-
-
+    results = session.query(*sel).\
+    filter(Measurement.date >= start).\
+    filter(Measurement.date <= end).all()
+    temps = list(np.ravel(results))
+    return jsonify(temps=temps)
